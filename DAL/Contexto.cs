ec543593862260f9ea11516912739bc69b5d4331
jimmy057 <jimmypolanco2540@gmail.com>
2025-03-10 15:26:25 -0400

    Contexto creado

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using JimmyConcepcion_P2_AP1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JimmyConcepcion_P2_AP1.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+         
+        public DbSet<Registro> registro { get; set; }
+    }
+}
