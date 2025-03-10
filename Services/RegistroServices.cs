2005e602a20c858538753bc540bcbb09103e254a
jimmy057 <jimmypolanco2540@gmail.com>
2025-03-10 15:31:42 -0400

    recibir el contexto en el services

--- Services/RegistroServices.cs
+++ Services/RegistroServices.cs
@@ -1,6 +1,10 @@
-﻿namespace JimmyConcepcion_P2_AP1.Services
+﻿using JimmyConcepcion_P2_AP1.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace JimmyConcepcion_P2_AP1.Services
 {
-    public class RegistroServices
+    public class RegistroServices(IDbContextFactory<Contexto> DbFactory)
     {
+
     }
 }
